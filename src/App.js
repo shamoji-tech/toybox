9c80e1090bf594ab3fc8cb083b493c4872a9ba11
shamoji-tech <menushark@gmail.com>
2021-08-19 04:26:26 +0900

    add drawer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,172 @@
-import React, { Component } from 'react';
-import NavBar from './components/NavBar';
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ContactSupportIcon from '@material-ui/icons/ContactSupport';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import AppsIcon from '@material-ui/icons/Apps';
 import Routes from './routes/Routes';
-import MenuDrawer from './components/MenuDrawer';
+import { Link } from 'react-router-dom';
 
-class App extends Component{
-    render() {
-        return (
-            <div>
-                <div>
 
-                </div>
-                <div>
-                    <MenuDrawer />
-                </div>
-                <div>
-                    <Routes />
-                </div>
-            </div>
-        );
-    }
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+export default function MiniDrawer() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Button color="inherit" component={Link} to="/">Shamoji101's ToyBox</Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          
+            <ListItem button component={Link} to="/counter">
+              <ListItemIcon><AddBoxIcon /></ListItemIcon>
+              <ListItemText primary="Counter" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon><AppsIcon /></ListItemIcon>
+              <ListItemText primary="Calculator" />
+            </ListItem>
+        </List>
+        <Divider />
+        <List>
+            <ListItem button>
+              <ListItemIcon><ContactSupportIcon /></ListItemIcon>
+              <ListItemText primary="Contact" />
+            </ListItem>
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <div>
+            <Routes />
+        </div>
+      </main>
+    </div>
+  );
 }
-
-export default App
--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,42 @@
 export const TYPE_INCREMENT = "@@shamoji/increment";
 export const TYPE_DECREMENT = "@@shamoji/decrement";
+export const TYPE_CALC_NUMBTN = "@@shamoji/calc_NumBtn";
+export const TYPE_CALC_PLUSBTN = "@@shamoji/calc_PlusBtn";
+export const TYPE_CALC_MINUSBTN = "@@shamoji/calc_MinusBtn";
+export const TYPE_CALC_TIMESBTN = "@@shamoji/calc_TimesBtn";
+export const TYPE_CALC_DIVBTN = "@@shamoji/calc_DivBtn";
+export const TYPE_CALC_EQUALBTN = "@@shamoji/calc_EqualBtn";
+
+
 export const increment = () =>({
     type: TYPE_INCREMENT,
 });
+
 export const decrement = () =>({
     type: TYPE_DECREMENT,
 });
+
+export const pushNumber = (number) =>({
+    type: TYPE_CALC_NUMBTN,
+    number,
+})
+
+export const pushPlus = () =>({
+    type: TYPE_CALC_PLUSBTN,
+})
+
+export const pushMinus = () =>({
+    type: TYPE_CALC_MINUSBTN,
+})
+
+export const pushTimes = () =>({
+    type: TYPE_CALC_TIMESBTN,
+})
+
+export const pushDiv = () =>({
+    type: TYPE_CALC_DIVBTN,
+})
+
+export const pushEqual = () =>({
+    type: TYPE_CALC_EQUALBTN,
+})
