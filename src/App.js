522e590ae4fbe186f335ff06597c5efcd5371f72
shamoji-tech <menushark@gmail.com>
2021-08-19 03:29:51 +0900

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 import Routes from './routes/Routes';
+import MenuDrawer from './components/MenuDrawer';
 
 class App extends Component{
     render() {
         return (
             <div>
                 <div>
-                    <NavBar />
+
+                </div>
+                <div>
+                    <MenuDrawer />
+                </div>
+                <div>
                     <Routes />
                 </div>
             </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from 'react-router-dom';
 
-class NavBar extends Component{
-    render(){
-        return (
-            <div>
-                <Link to="/">Home</Link>/
-                <Link to="/counter">Counter</Link>/
-            </div>
-        );
-    }
-}
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+}));
 
-export default NavBar
+export default function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Button color="inherit" component={Link} to="/">
+            Hello
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
