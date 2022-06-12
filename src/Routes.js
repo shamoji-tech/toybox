759dcbcfac0da294e8ce14ddb5ea8f5f16acdf63
shamoji-tech <menushark@gmail.com>
2022-06-12 16:00:03 +0900

    remove contact from Routes

--- src/Routes.js
+++ src/Routes.js
@@ -5,7 +5,6 @@ import Counter from './components/Counter/Counter';
 import Calculator from './components/Calculator/Calculator';
 import Timer from './components/Timer/Timer';
 import MineSweeper from './components/MineSweeper/MineSweeper';
-import Contact from './components/Contact/Contact';
 
 
 class Routes extends Component {
@@ -17,7 +16,6 @@ class Routes extends Component {
                 <Route path="/calc" component={Calculator} />
                 <Route path="/timer" component={Timer} />
                 <Route path="/mine_sweeper" component={MineSweeper} />
-                <Route path="/contact" component={Contact} />
             </Switch>
         );
     }
